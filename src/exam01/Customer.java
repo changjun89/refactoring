861d551207a6052a9848448180cab5c55d598e5d
DESKTOP-49C2M5J\LEE <leechang0423@naver.com>
2019-03-19 21:38:13 +0900

    step6. 적립포인트 계산 메소드로 분리

--- src/exam01/Customer.java
+++ src/exam01/Customer.java
@@ -25,17 +25,12 @@ public class Customer {
         Enumeration rentals = _rentals.elements();
         String result = getName() + "고객님의 대여 기록\n";
         while (rentals.hasMoreElements()) {
-            double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
 
-            //비디오 종류별 대여료 계산
-            //적립 포인트를 1 포인트 증가
-            frequentRenterPoints++;
 
-            //최신물을 이틀 이상 대여하면 보너스 포인트 지급
-            if (each.getMovie().getPriceCode() == Movie.NEW_RELEASE && each.getDaysRented() > 1) {
-                frequentRenterPoints++;
-            }
+            //경우에 따른 적립 포인트지급 함수를 호출
+            frequentRenterPoints += each.getFrequentRenterPoints();
+
 
             //이번에 대여하는 비디오 정보와 대여료를 출력
             result += "\t" + each.getMovie().getTitle() + "\t" +
